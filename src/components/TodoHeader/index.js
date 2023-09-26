28193c34db9c7cd216f3a7a0cba5f7dc87a55c6f
NTD567 <ntdat567@gmail.com>
2023-09-26 23:59:50 +0700

    lesson-3

---
+++ src/components/TodoHeader/index.js
@@ -0,0 +1,8 @@
+const TodoHeader = () => {
+    return ( <div className="todo-header">
+       <input placeholder="Enter your task here ..."></input>
+    </div>
+    );
+   };
+   
+   export default TodoHeader;
